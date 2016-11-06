0f8c909a24f2a9e9380b130458ea99ddb0c09479
zwiepsel <zwiepsel@gmail.com>
2016-11-06 16:24:50 +0100

    aanpassingen

--- scripts.js
+++ scripts.js
@@ -1,8 +1,9 @@
   var app = angular.module('archilogus', [])
     .controller('configuratorController', function ($scope) {
-
       var carport = false;
       var garage = false;
+      var old = false;
+      var wide = false;
       $scope.extras = [];
       $scope.totalPrice = 200000;
 
@@ -14,6 +15,20 @@
         }
         throw "Couldn't find object with id: " + id;
       }
+
+      $scope.requestQuote = function(){
+        console.log($scope.user.name + " " + $scope.user.email)
+        
+      }
+      $('#email').validator().on('submit', function (e) {
+        console.log('crap')
+        if (e.isDefaultPrevented()) {
+          console.log('bad')
+        } else {
+          console.log('good')
+          // everything looks good!
+        }
+      })
       //switch between all the extras
       $scope.addExtras = function (value) {
         switch (value) {
@@ -21,7 +36,8 @@
             if (!carport) {
               $scope.extras.push({
                 id: 1,
-                description: "Carport €25.000"
+                description: "Carport",
+                price: 25000
               })
               $scope.totalPrice = $scope.totalPrice + 25000
               carport = true;
@@ -32,10 +48,35 @@
             if (!garage) {
               $scope.extras.push({
                 id: 2,
-                description: "Garage €25.000"
+                description: "Garage",
+                price: 25000
               })
               $scope.totalPrice = $scope.totalPrice + 25000
               garage = true;
+              break;
+            }
+            break;
+          case 3:
+            if (!old) {
+              $scope.extras.push({
+                id: 3,
+                description: "65+ aanpassing",
+                price: 25000
+              })
+              $scope.totalPrice = $scope.totalPrice + 25000
+              old = true;
+              break;
+            }
+            break;
+          case 4:
+            if (!wide) {
+              $scope.extras.push({
+                id: 4,
+                description: "Verbrede woonkamer",
+                price: 25000
+              })
+              $scope.totalPrice = $scope.totalPrice + 25000
+              wide = true;
               break;
             }
             break;
@@ -54,16 +95,34 @@
               $scope.extras.splice(index, 1)
               $scope.totalPrice = $scope.totalPrice - 25000
               carport = false;
+               break;
             }
             break;
-
           case 2:
             if (garage) {
               index = $scope.extras.indexOf(findById($scope.extras, value))
               $scope.extras.splice(index, 1)
               $scope.totalPrice = $scope.totalPrice - 25000
               garage = false;
-              break;
+               break;
+            }
+            break;
+          case 3:
+            if (old) {
+              index = $scope.extras.indexOf(findById($scope.extras, value))
+              $scope.extras.splice(index, 1)
+              $scope.totalPrice = $scope.totalPrice - 25000
+              old = false;
+               break;
+            }
+            break;
+        case 4:
+            if (wide) {
+              index = $scope.extras.indexOf(findById($scope.extras, value))
+              $scope.extras.splice(index, 1)
+              $scope.totalPrice = $scope.totalPrice - 25000
+              wide = false;
+               break;
             }
           default:
             break;
@@ -74,12 +133,7 @@
   angular.bootstrap(document, ['archilogus']);
 
   //checkbox by code
-  $('#input-1').checkboxpicker({
-    html: true,
-    offLabel: '<span class="glyphicon glyphicon-remove">',
-    onLabel: '<span class="glyphicon glyphicon-ok">',
-  });
-
+  
   $('.next').click(function () {
     var nextId = $(this).parents('.tab-pane').next().attr("id");
     $('.nav a[href="#' + nextId + '"]').tab('show')
@@ -97,12 +151,12 @@
 
     //update progress
     var stap = $(e.target).data('stap');
-    var percent = (parseInt(stap) / 8) * 100;
+    var percent = (parseInt(stap) / 5) * 100;
 
     $('.progress-bar').css({
       width: percent + '%'
     });
-    $('.progress-bar').text("Stap " + stap + " of 8");
+    $('.progress-bar').text("Stap " + stap + " van 5");
 
     //e.relatedTarget // previous tab
 
